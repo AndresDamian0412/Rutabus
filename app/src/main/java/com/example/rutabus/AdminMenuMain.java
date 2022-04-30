f5d15579ec3313ecab14f203f34174a66d75e418
BNN0 <joseph2010.jalh@gmail.com>
2022-04-30 02:42:50 -0500

    Un desmadre se acaba de subir jaja de nuevo xd x2

--- app/src/main/java/com/example/rutabus/AdminMenuMain.java
+++ app/src/main/java/com/example/rutabus/AdminMenuMain.java
@@ -1,16 +1,22 @@
 package com.example.rutabus;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminMenuMain extends AppCompatActivity {
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu_main);
-        
+
+        backButton = findViewById(R.id.back_btn);
+        backButton.setOnClickListener(view -> finish());
     }
 }
