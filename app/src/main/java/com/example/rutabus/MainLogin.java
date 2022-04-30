31c3a2bd3e378d4532981d051a4b081af5b8ae48
BNN0 <joseph2010.jalh@gmail.com>
2022-04-30 17:35:20 -0500

    Un desmadre se acaba de subir jaja de nuevo xd x2

--- app/src/main/java/com/example/rutabus/MainLogin.java
+++ app/src/main/java/com/example/rutabus/MainLogin.java
@@ -1,13 +1,38 @@
 package com.example.rutabus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainLogin extends AppCompatActivity {
+
+    Button UserBtn;
+    LinearLayout DriverBtn, AdminBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        UserBtn = findViewById(R.id.user_view_btn);
+        DriverBtn = findViewById(R.id.driver_view_btn);
+        AdminBtn = findViewById(R.id.admin_view_btn);
+        UserBtn.setOnClickListener(view -> switchUserView());
+        DriverBtn.setOnClickListener(view -> switchDriverView());
+        AdminBtn.setOnClickListener(view -> switchAdminView());
+    }
+    private void switchUserView(){
+        Intent switchActivity = new Intent(this, Activity_mainview_user.class);
+        startActivity(switchActivity);
+    }
+    private void switchDriverView(){
+        Intent switchActivity = new Intent(this, DriverLogin.class);
+        startActivity(switchActivity);
+    }
+    private void switchAdminView(){
+        Intent switchActivity = new Intent(this, AdminLogin.class);
+        startActivity(switchActivity);
     }
 }
