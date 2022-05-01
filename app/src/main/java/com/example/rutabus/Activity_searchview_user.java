cd5864fd0ed5e280d9c2ee5dab2e6cb9e6c5405f
AndresDamian0412 <andres.mc.damian@gmail.com>
2022-04-30 23:54:26 -0500

    Adaptador para el Recycler

--- app/src/main/java/com/example/rutabus/Activity_searchview_user.java
+++ app/src/main/java/com/example/rutabus/Activity_searchview_user.java
@@ -2,12 +2,27 @@ package com.example.rutabus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Activity_searchview_user extends AppCompatActivity {
 
+    ArrayList<String> listDatos;
+    RecyclerView recycler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchview_user);
+
+        recycler = (RecyclerView) findViewById(R.id.recycler_v);
+        recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        listDatos = new ArrayList<String>();
+        listDatos.add("TEC - VALLE ALTO");
+        listDatos.add("TEC - PASR");
+
+        AdapterDatos adapter = new AdapterDatos(listDatos);
     }
 }
