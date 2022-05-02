23355e109111bf97e29ea6d7ed5838513364ffa5
BNN0 <joseph2010.jalh@gmail.com>
2022-05-02 12:54:13 -0500

    Un desmadre se acaba de subir jaja de nuevo xd x4 jja

--- app/src/main/java/com/example/rutabus/AdminMenuMain.java
+++ app/src/main/java/com/example/rutabus/AdminMenuMain.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class AdminMenuMain extends AppCompatActivity {
     ImageView backButton;
+    LinearLayout paradasView, rutasView, driversView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,24 @@ public class AdminMenuMain extends AppCompatActivity {
         setContentView(R.layout.activity_admin_menu_main);
 
         backButton = findViewById(R.id.back_btn);
+        paradasView = findViewById(R.id.paradas_btn);
+        rutasView = findViewById(R.id.rutas_btn);
+        driversView = findViewById(R.id.drivers_btn);
         backButton.setOnClickListener(view -> finish());
+        paradasView.setOnClickListener(view -> switchParadasMenu());
+        rutasView.setOnClickListener(view -> switchRutasMenu());
+        driversView.setOnClickListener(view -> switchDriversMenu());
+    }
+    private void switchParadasMenu(){
+        Intent switchParadas = new Intent(this, AdminMenuParadas.class);
+        startActivity(switchParadas);
+    }
+    private void switchRutasMenu(){
+        Intent switchRutas = new Intent(this, AdminMenuRutas.class);
+        startActivity(switchRutas);
+    }
+    private void switchDriversMenu(){
+        Intent switchDrivers = new Intent(this, AdminMenuDrivers.class);
+        startActivity(switchDrivers);
     }
 }
