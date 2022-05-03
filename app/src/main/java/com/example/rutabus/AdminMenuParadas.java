31abd0501a5668f937086711b4ee292f418c3004
BNN0 <joseph2010.jalh@gmail.com>
2022-05-02 21:49:57 -0500

    Un desmadre se acaba de subir jaja de nuevo xd x4 jja

--- app/src/main/java/com/example/rutabus/AdminMenuParadas.java
+++ app/src/main/java/com/example/rutabus/AdminMenuParadas.java
@@ -2,11 +2,16 @@ package com.example.rutabus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class AdminMenuParadas extends AppCompatActivity {
+    ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu_paradas);
+
+        backButton = findViewById(R.id.back_btn);
+        backButton.setOnClickListener(view -> finish());
     }
 }
